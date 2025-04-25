f742a13c065ea02da15ff7ef6b6ee5657ae7cb87
ompa <nmaladina@hotmail.com>
2025-04-26 08:34:24 +1200

    added css moduel support with vite

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,13 @@
+import classes from'./Post.module.css'
 
 
 const Post = (props) => {
     props.author,
     props.body
   return (
-    <div>
-    <h1>{props.author}</h1>
-    <h1>{props.body}</h1>
+    <div className={classes.post}>
+    <p className={classes.author}> {props.author}</p>
+    <p className={classes.text}>{props.body}</p>
     </div>
   )
 }
