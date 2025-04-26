75fe1e59b5bacdd27dd1c7fa346b5a58f8a1cbdf
ompa <nmaladina@hotmail.com>
2025-04-26 20:49:57 +1200

    working with state

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,16 +1,23 @@
+
+import { useState } from 'react';
+
 import classes from './NewPost.module.css';
 
 function NewPost() {
-    function changeBodyHandler(e) {
-        console.log(e.target.value);
+    const [enteredBody, setEnteredBody ] = useState('');
+    
+    
+    function changeBodyHandler(event) {
+        setEnteredBody(event.target.value);
         
     }
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange ={changeBodyHandler}/>
+        <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
       </p>
+      <p className={classes.name}>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
