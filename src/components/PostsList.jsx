a121a2f85026377a949495835192167cc092e62c
ompa <nmaladina@hotmail.com>
2025-04-26 20:09:21 +1200

    preparing for state management

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
+import NewPost from './NewPost';
 import Post from './Post'
 import classes from './PostList.module.css'
 
 function PostList() {
     return (
+        <>
+        <NewPost />
         <ul className={classes.posts}>
             <Post  author = "Ompa" body ="React.js is Awesome!" />
             <Post author = "Dupsy" body ="Check out my React Life" />
         </ul>
+        </>
     )
 }
 
