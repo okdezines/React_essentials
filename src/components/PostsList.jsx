9822a2b9213c346827fcb72ddff21d315cd93e12
ompa <nmaladina@hotmail.com>
2025-04-27 12:09:40 +1200

    adding a shared Header & More state management

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,15 +4,12 @@ import NewPost from './NewPost';
 import Post from './Post'
 import classes from './PostList.module.css'
 
-function PostList() {
-    const [modalIsVisible, setModalIsVisible]  = useState(true)
+function PostList({isPosting, onStopPosting}) {
     const [enteredBody, setEnteredBody] = useState('')
     const [enteredAuthor, setEnteredAuthor] = useState('')
     
     //HOOD functions
-    function hideModalHandler(){
-        setModalIsVisible(false)
-    }
+ 
     function bodyChangeHandler(event) {
         setEnteredBody(event.target.value)
     }
@@ -23,8 +20,8 @@ function PostList() {
     return (
         <>
         
-        {modalIsVisible && (
-         <Modal onClose={hideModalHandler}>
+        {isPosting && (
+         <Modal onClose={onStopPosting}>
             <NewPost 
                 
             onBodyChange={bodyChangeHandler} 
