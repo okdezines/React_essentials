70a6254dd2264b723f8f9888bbae4cd688f4ad3d
ompa <nmaladina@hotmail.com>
2025-05-18 21:21:51 +1200

    handling loading state

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,17 +6,20 @@ import classes from './PostList.module.css'
 
 function PostList({isPosting, onStopPosting}) { 
     const [posts, setPosts] = useState([])
+    const [isFetching, setIsFetching] = useState(false)
     
     useEffect(() => {
         async function fetchPosts() {
+            setIsFetching(true);
             const response = await fetch('http://localhost:8080/posts');
-            const resdata = await response.json();
-            setPosts(resdata.posts);
+            const data = await response.json();
+            setPosts(data.posts);
+            setIsFetching(false);
         }
         fetchPosts()
-            .catch((error) => {
-                console.log(error);
-            });
+            // .catch((error) => {
+            //     console.log(error);
+            // });
         
     }, []);
                                          
@@ -33,14 +36,14 @@ function PostList({isPosting, onStopPosting}) {
     
     return (
         <>
-        {isPosting && (
+        { isPosting && (
          <Modal onClose={onStopPosting}>
             <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} 
              />
          </Modal>
         )}   
         <h2>Posts</h2>
-        {posts.length > 0 && (
+        {!isFetching && posts.length > 0 && (
             <ul className={classes.posts}>
             {posts.map((post) => 
                 <Post
@@ -50,7 +53,12 @@ function PostList({isPosting, onStopPosting}) {
                     )}
             </ul>
         )}
-        {posts.length === 0 && <p style={{textAlign:'center', color: 'white'}}>No posts yet!</p>}
+        {!isFetching && posts.length === 0 && (
+            <p style={{textAlign:'center', color: 'white'}}>No posts yet!</p>)}
+        {isFetching && (
+            <div style={{textAlign:'center', color: 'white'}}>
+            <p>Loading...</p></div>
+            )}
         </>
     );
 }
