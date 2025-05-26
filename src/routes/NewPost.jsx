5c509bdcc6e36800340ebf8d1e8b27cc1df7d652
ompa <nmaladina@hotmail.com>
2025-05-26 12:02:56 +1200

    Linking & Navigating

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import classes from "./NewPost.module.css";
 import Modal from "../components/Modal";
+import {Link} from "react-router-dom";
 
-function NewPost({ onCancel, onAddPost }) {
+function NewPost({ onAddPost }) {
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
 
@@ -37,9 +38,9 @@ function NewPost({ onCancel, onAddPost }) {
         <input type="text" id="name" required onChange={authorChangeHandler} />
       </p>
       <p className={classes.actions}>
-        <button type="button" onClick={onCancel}>
+        <Link to='/' type="button">
           Cancel
-        </button>
+        </Link>
         <button>Submit</button>
       </p>
     </form>
